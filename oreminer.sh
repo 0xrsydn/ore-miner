693c6fa265eaa5e797afdb71d27c551af2c6e4c5
Rasyidan Akbar F <78522797+0xrsydn@users.noreply.github.com>
2024-04-05 09:18:29 +0700

    Update oreminer.sh

--- oreminer.sh
+++ oreminer.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-while true
-do
-  echo "Running"
-  ore --rpc <ALCHEMY_RPC_FROM_STEP_7> --keypair ~/.config/solana/id.json --priority-fee 10 mine --threads 4
-  echo "Exited"
+#add your solana rpc endpoint
+DEFAULT_RPC_URL="......"
+#add your solana pubkey path
+DEFAULT_KEY="....."
+#define solana gas fee
+DEFAULT_FEE=0
+#define how many cpu threads used
+DEFAULT_THREADS=10
+
+RPC_URL=${1:-$DEFAULT_RPC_URL}
+KEY=${2:-$DEFAULT_KEY}
+FEE=${3:-$DEFAULT_FEE}
+THREADS=${4:-$DEFAULT_THREADS}
+
+COMMAND="./target/release/ore --rpc ${RPC_URL} --keypair ${KEY} --priority-fee ${FEE} mine --threads ${THREADS}"
+
+while true; do
+  echo "Starting the process..."
+  eval $COMMAND
+  [ $? -eq 0 ] && break
+  echo "Restart in 5 seconds..."
+  sleep 5
 done
